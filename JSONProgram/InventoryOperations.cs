564f241e482a8e5321769e9eb4c83388c0cf65e5
mahesh <lokilokesh1680@gmail.com>
2023-03-16 14:43:04 +0530

    [lokesh]add:folder is added

---
+++ JSONProgram/InventoryOperations.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONProgram
+{
+    public class InventoryOperation
+    {
+        public void ReadJsonFile(string Filepath)
+        {
+            var data=File.ReadAllText(Filepath);
+            var result=JsonConvert.DeserializeObject<List<InventaryData>>(data);
+            foreach (var inventary in result)
+            {
+                Console.WriteLine(inventary.Name + "\t" + inventary.Weight + "\t" + inventary.PricePerkg + "\t" + inventary.Weight * inventary.pricePerkg);
+            }
+        }
+
+    }
+}
--- JSONProgram/Program.cs
+++ JSONProgram/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace JSONProgram
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            InventoryOperation inventory = new InventoryOperation();
+            inventory.ReadJsonFile(@"D:\BL excercise\JSONProgram\JSONProgram\InventaryDataManagement\Inventory.Json");
+        }
+    }
+}
